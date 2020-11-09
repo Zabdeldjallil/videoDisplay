a799d3bc48031ffcf9cc9f24dd1eedd0eb5aa325
djallil <djillou500@gmail.com>
2020-11-09 21:12:29 +0100

    upload/create bdd/upload page

--- app.js
+++ app.js
@@ -1,12 +1,59 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('TubeYou', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'  
+});
 import express from 'express';
-import {extname,resolve,join} from "path"
-import {createReadStream,stat,statSync} from 'fs'
-import {promisify} from "util"
+import path from "path";
+import {createReadStream,stat,statSync} from 'fs';
+import {promisify} from "util";
 import ejs from 'ejs';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import multer from "multer";
+
+const Video = sequelize.define('Video', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  pathVid: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  pathImg:{
+      type:DataTypes.STRING,
+      allowNull:false
+  },
+},{
+  });
+
+/*(async function(){
+    await Video.sync({});
+    console.log("The table for the User model was just (re)created!");
+  })()*/
+
+
+//var upload = multer({ dest: 'public/uploads' })
 const app=new express();
 app.use(express.static("public"))
 app.set("view engine",'ejs')
+
+app.get("/uploading",function(req,res){
+  res.render("uploading")
+})
+app.post('/uploading', function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+  upload2(req,res,(err) =>{
+    if(err)res.send("err");
+    else{
+      if(req.file==undefined)res.send("Undefined")
+      else res.redirect("/uploading")
+    }
+  })
+})
 app.get("/accueil",function(req,res){
   res.render("accueil")
 })
@@ -55,5 +102,34 @@ app.get("/video/:vid",function(req,res){
     }
 })
 
+const storage2 = multer.diskStorage({
+  destination: './public',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+// Init Upload
+const upload2 = multer({
+  storage: storage2,
+  limits:{fileSize: 10000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('video');
+// Check File Type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /mp4|mov/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Videos Only!');
+  }
+}
 
 app.listen(8080,function(){console.log("started listening on port 8080")})
